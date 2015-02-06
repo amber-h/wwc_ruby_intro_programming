5eb176cb96c88e09651b54985b3386c905442146
laceylaborde <laceylaborde@gmail.com>
2015-02-06 13:23:32 -0600

    update temperature problems

--- 01_temperature/temperature_spec.rb
+++ 01_temperature/temperature_spec.rb
@@ -1,20 +1,3 @@
-# # Topics:
-# * functions
-# * floating-point math
-# * arithmetic functions
-# # Hints
-#
-# Remember that one degree fahrenheit is 5/9 of one degree celsius, and that the freezing point of water is 0 degrees celsius but 32 degrees fahrenheit.
-#
-# In integer math, there **are no fractions**. So if you are using integer literals, you will be using integer math, which means, for example...
-#
-#    1 / 2 => 0
-#
-# In floating point math, there **are** fractions. So...
-#
-#    1.0 / 2.0 => 0.5
-#
-
 require "temperature"
 
 describe "temperature conversion functions" do
@@ -33,8 +16,8 @@ describe "temperature conversion functions" do
       fahrenheit_to_celsius(98.6).should == 37
     end
 
-    it "converts arbitrary temperature" do
-      fahrenheit_to_celsius(68).should == 20
+    it "converts a temperature and rounds to 1 decimal place" do
+      fahrenheit_to_celsius(51).should == 10.6
     end
 
   end
@@ -49,17 +32,8 @@ describe "temperature conversion functions" do
       celsius_to_fahrenheit(100).should == 212
     end
 
-    it "converts arbitrary temperature" do
-      celsius_to_fahrenheit(20).should == 68
-    end
-
-    it "converts body temperature" do
-      celsius_to_fahrenheit(37).should be_within(0.1).of(98.6)
-      # Why do we need to use be_within?
-      # See http://www.ruby-forum.com/topic/169330
-      # and http://en.wikipedia.org/wiki/IEEE_754-2008
-      # and http://en.wikipedia.org/wiki/Double_precision_floating-point_format
-      # Also, try "puts 0.5 - 0.4 - 0.1" -- pretty crazy, right?
+    it "converts a temperature and rounds to 1 decimal place" do
+      celsius_to_fahrenheit(43).should == 19.8
     end
 
   end
