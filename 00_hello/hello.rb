f788b42ea66907905a36ebf6f66d59b225c83705
amber <amber.houle3@gmail.com>
2015-02-05 23:56:31 -0600

    Amber - adding test case to hello_spec.rb and adding template empty methods in hello.rb

---
+++ 00_hello/hello.rb
@@ -0,0 +1,11 @@
+def hello
+
+end
+
+def greet(name)
+
+end
+
+def calculate_age(date_of_birth)
+
+end
--- 00_hello/hello_spec.rb
+++ 00_hello/hello_spec.rb
@@ -10,7 +10,13 @@ describe "the greet function" do
   it "says hello to someone" do
     greet("Alice").should == "Hello, Alice!"
   end
+  it "says hello to someone else" do
+    greet("Bob").should == "Hello, Bob!"
+  end
 
+end
+
+describe "the calculate age function" do
   it "tells someone their age" do
     calculate_age(1992) == 23
   end
